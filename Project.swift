0b37a31bb5dc8edb3d9a64071b4c9c41f0470168
Insu Byeon <yuki@yuki.kr>
2021-12-04 21:36:36 +0900

    LaunchScreen 배경색 수정 및 컴파일 가능하도록 수정

--- Project.swift
+++ Project.swift
@@ -1,25 +1,10 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
-/*
-                +-------------+
-                |             |
-                |     App     | Contains NaverMovie App target and NaverMovie unit-test target
-                |             |
-         +------+-------------+-------+
-         |         depends on         |
-         |                            |
- +----v-----+                   +-----v-----+
- |          |                   |           |
- |   Kit    |                   |     UI    |   Two independent frameworks to share code and start modularising your app
- |          |                   |           |
- +----------+                   +-----------+
-
- */
-
 // MARK: - Project
 
-// Creates our project using a helper function defined in ProjectDescriptionHelpers
-let project = Project.app(name: "NaverMovie",
-                          platform: .iOS,
-                          additionalTargets: ["NaverMovieKit", "NaverMovieUI"])
+let project = Project.app(
+  name: "NaverMovie",
+  platform: .iOS,
+  additionalTargets: []
+)
