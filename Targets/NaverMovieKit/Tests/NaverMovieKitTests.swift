ee6e85e9fa105774df2bba2875b3582f546bcde8
Insu Byeon <yuki@yuki.kr>
2021-12-04 20:33:47 +0900

    tuist project setup

---
+++ Targets/NaverMovieKit/Tests/NaverMovieKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class NaverMovieKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("NaverMovieKit", "NaverMovieKit")
+    }
+}
---
+++ Targets/NaverMovieUI/Tests/NaverMovieUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class NaverMovieUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("NaverMovieUI", "NaverMovieUI")
+    }
+}
